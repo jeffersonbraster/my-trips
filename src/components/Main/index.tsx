75f45a6b4f278bccf73935db35294a906d924926
Jefferson <jeffersonbraster@gmail.com>
2021-03-03 20:20:35 -0300

    iniciando projeto

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,3 @@
-import * as S from './styles'
-
-const Main = () => (
-  <S.Wrapper>
-    <S.Logo src="/img/logo.svg" alt="logo" />
-    <S.Title>Next Boilerplate</S.Title>
-    <S.Description>
-      Typescript, ReactJs, NextJs e Styled-components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="um desenvolvedor de frente a tela com codigo"
-    />
-  </S.Wrapper>
-)
+const Main = () => <h1>next js</h1>
 
 export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,14 +7,14 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next - Boilerplate</title>
+        <title>My-Trips</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092b" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple project to show my favorites spots."
         />
       </Head>
       <GlobalStyles />
