384b3276e52a3f5361b6a4b422e21f832e7a5925
Jefferson <jeffersonbraster@gmail.com>
2021-03-03 19:52:49 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="logo" />
+    <S.Title>Next Boilerplate</S.Title>
+    <S.Description>
+      Typescript, ReactJs, NextJs e Styled-components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="um desenvolvedor de frente a tela com codigo"
+    />
+  </S.Wrapper>
+)
+
+export default Main
