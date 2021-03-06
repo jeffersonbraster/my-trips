bef3252bfe8d24cd7f01d2e920c6c8fda3715152
Jefferson <jeffersonbraster@gmail.com>
2021-03-06 12:33:13 -0300

    configurando next seo

---
+++ next-seo.config.ts
@@ -0,0 +1,8 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://my-trips.jeffersonbrandao.com.br',
+    site_name: 'My Trips'
+  }
+}
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import LinkWrapper from 'components/LinkWrapper'
 import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 import dynamic from 'next/dynamic'
@@ -8,6 +9,26 @@ const Map = dynamic(() => import('components/Map'), { ssr: false })
 export default function HomeTemplate({ places }: MapProps) {
   return (
     <>
+      <NextSeo
+        title="My trips"
+        description="Um simples projeto que mostra minhas viagens pelas praias do nordeste."
+        canonical="https://my-trips.jeffersonbrandao.com.br"
+        openGraph={{
+          url: 'https://my-trips.jeffersonbrandao.com.br',
+          title: 'My Trips',
+          description:
+            'Um simples projeto que mostra minhas viagens pelas praias do nordeste.',
+          images: [
+            {
+              url: 'https://my-trips.jeffersonbrandao.com.br/img/cover.png',
+              width: 1280,
+              height: 720,
+              alt: 'My Trips'
+            }
+          ],
+          site_name: 'My Trips'
+        }}
+      />
       <LinkWrapper href="/about">
         <InfoOutline size={32} aria-label="About" />
       </LinkWrapper>
