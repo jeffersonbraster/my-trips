95b21ac5e99e88520a3cd3cf1b15ae7e2dc9a328
Jefferson <jeffersonbraster@gmail.com>
2021-03-16 20:30:40 -0300

    add google search console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="putVCDNpRngXtHqWSW8KORYYnPFCT9TeOS8O6ow0-9Q"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
